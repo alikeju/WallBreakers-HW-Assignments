e3956aedd80543ef3a673f4367f53b904382fd2c
alikeju <alikejuadejo@yahoo.com>
2020-02-21 16:24:44 -0500

    Adds 1 problem

--- src/com/company/hw_3.java
+++ src/com/company/hw_3.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.*;
 
 public class hw_3 {
     /*
@@ -17,5 +18,24 @@ public class hw_3 {
             }
         }
         return true;
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int a_pointer = 0;
+        int b_pointer = 0;
+
+        while (a_pointer < g.length && b_pointer < s.length){
+            if (g[a_pointer] <= s[b_pointer]){
+                a_pointer++;
+                b_pointer++;
+            } else {
+                b_pointer++;
+            }
+        }
+
+        return a_pointer;
     }
 }

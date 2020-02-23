5cad072d8cbfa5cefebb228ed6168431a26a3ff3
alikeju <alikejuadejo@yahoo.com>
2020-02-23 15:31:23 -0500

    Adds 1 problem

--- src/com/company/hw_3.java
+++ src/com/company/hw_3.java
@@ -104,4 +104,15 @@ public class hw_3 {
 
         return answer;
     }
+
+    public int search(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }

30614f05d15aacd5ada4cb8bf52899c93e3a165b
alikeju <alikejuadejo@yahoo.com>
2020-03-03 23:51:36 -0500

    Adds Problems

--- src/com/company/hw_4.java
+++ src/com/company/hw_4.java
@@ -29,5 +29,124 @@ public class hw_4 {
 
         return score;
     }
-    
+
+    /*
+        First Approach
+     */
+
+    public boolean isValid2(String s) {
+        int lefParen = 0;
+        int rightParen = 0;
+        int lefCurly = 0;
+        int rightCurly = 0;
+        int lefBrace = 0;
+        int rightBrace = 0;
+
+        for (char c: s.toCharArray()) {
+            if (c == '(') {
+                lefParen++;
+            } else if (c == ')') {
+                rightParen++;
+            } else if (c == '{') {
+                lefCurly++;
+            } else if (c == '}') {
+                rightCurly++;
+            } else if (c == '[') {
+                lefBrace++;
+            } else {
+                rightBrace++;
+            }
+        }
+
+        if (lefParen == rightParen && lefCurly == rightCurly && lefBrace == rightBrace) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+        Correct Answer
+     */
+
+    private HashMap<Character, Character> mappings;
+
+    public hw_4() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char paren: s.toCharArray()){
+            if (this.mappings.containsKey(paren)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if (topElement != this.mappings.get(paren)) {
+                    return false;
+                }
+
+            } else {
+                stack.push(paren);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public boolean backspaceCompare(String S, String T) {
+        Stack<Character> stackS = new Stack<>();
+        Stack<Character> stackT = new Stack<>();
+
+        for (char c: S.toCharArray()) {
+            if (c == '#')  {
+                if (!stackS.isEmpty()){
+                    stackS.pop();
+                }
+            } else {
+                stackS.push(c);
+            }
+        }
+
+        for (char c: T.toCharArray()) {
+            if (c == '#')  {
+                if (!stackT.isEmpty()){
+                    stackT.pop();
+                }
+            } else {
+                stackT.push(c);
+            }
+        }
+
+        return stackS.equals(stackT);
+
+    }
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] answer = new int[nums1.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < nums2.length; i++){
+            while(!stack.empty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+
+            stack.push(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++){
+            if (map.containsKey(nums1[i])) {
+                answer[i] = map.get(nums1[i]);
+            } else {
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
+
 }

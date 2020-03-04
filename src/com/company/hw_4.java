087ca67176a7e2c32c8d10d7ff35ceaf7219ad6f
alikeju <alikejuadejo@yahoo.com>
2020-03-04 15:03:52 -0500

    Adds 1 problem

--- src/com/company/hw_4.java
+++ src/com/company/hw_4.java
@@ -188,4 +188,44 @@ public class hw_4 {
         }
     }
 
+    class MyStack {
+
+        Queue<Integer> q1;
+        Queue<Integer> q2;
+
+        public MyStack() {
+            q1 = new LinkedList<>();
+            q2 = new LinkedList<>();
+        }
+
+        /** Push element x onto stack. */
+        public void push(int x) {
+            q2.add(x);
+
+            while(!q1.isEmpty()){
+                q2.add(q1.remove());
+            }
+
+            Queue<Integer> temp = q1;
+            q1 = q2;
+            q2 = temp;
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int pop() {
+            return q1.remove();
+        }
+
+        /** Get the top element. */
+        public int top() {
+            return q1.peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public boolean empty() {
+            return q1.isEmpty();
+        }
+    }
+
+
 }
